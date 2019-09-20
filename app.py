53fb5ca52efdaae3e18fefe7d62524ef5a8f933e
hopeper <55595021+hopeper@users.noreply.github.com>
2019-09-20 23:34:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,13 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    #sendText(user,userText)
-    if (userText == 'สวัสดี') :
-        sendText(user,'เออดีด้วย')
-    elif (userText == 'ไอ้บ้า') :
-        sendText(user,'เอ็งซิบ้า')
+     if (userText == 'สวัสดี') :
+        print('ดีจ้า')
+    elif (userText == 'สบายดีใหม') :
+        sendText(user,'สบายดีสบายมากเป็นอย่างเงี้ย')
+    else :
+        sendText(user,'อิหยาา')
+
     return '',200
 
 def sendText(user, text):
